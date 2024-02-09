22b36085c412ca34fb9ab231d8108a594aa3f878
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-02-09 18:43:26 +0530

    implement add department

--- src/components/dashboard/add_department.jsx
+++ src/components/dashboard/add_department.jsx
@@ -1,36 +1,91 @@
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import {
+  useCreateDepartmentMutation,
+  useGetAllDepartmentsQuery,
+} from "../../redux/features/department/departmentApi";
+import { options } from "./dashboard.utils";
 
 const AddDepartment = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+  
+  const [createDepartment] = useCreateDepartmentMutation();
+  const [departments, setDepartments] = useState([]);
+  const { data } = useGetAllDepartmentsQuery(undefined);
+  useEffect(() => {
+    if (data && Array.isArray(data.data)) {
+      setDepartments(data.data);
+    }
+  }, [data]);
+
+  const renderOptionsExist = (optionValue) => {
+    const existingDepartment = departments.find(
+      (department) => department.departmentName === optionValue
+    );
+    if (existingDepartment) {
+      return `${optionValue} (Department already exist)`;
+    }
+    return optionValue;
   };
 
-  console.log(watch("example"));
+  const isOptionDisabled = (optionValue) => {
+    return departments.find(
+      (department) => department.departmentName === optionValue
+    );
+  };
+
+  const onSubmit = async (data) => {
+    const res = await createDepartment(data);
+    if (res) {
+      toast(`${res.data}`, {
+        autoClose: 1000,
+        theme: "light",
+        type: "success",
+      });
+    }
+  };
+
   return (
     <div className="m-4">
       <h1 className="text-2xl">Add Department</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          type="text"
-          placeholder="Name"
-          {...register("name", { required: true })}
-          className="input input-bordered input-sm w-full max-w-xs mt-4"
-        />{" "}
+        <select
+          {...register("departmentName", { required: true })}
+          className="select select-bordered select-sm w-full max-w-xs mt-4"
+        >
+          <option value="" disabled selected>
+            Select Options
+          </option>
+          {options.map((option, i) => (
+            <option
+              key={i}
+              value={option.value}
+              disabled={isOptionDisabled(option.value)}
+            >
+              {renderOptionsExist(option.value)}
+            </option>
+          ))}
+        </select>
         <br />
-        {errors.name && (
-          <span className="text-red-500 text-sm">This Name is required</span>
-        )}{" "}
+        {errors.departmentName && (
+          <span className="text-red-500 text-sm">Please select an option</span>
+        )}
         <br />
-        <button type="submit" className="btn btn-sm btn-outline btn-success">
+        <button
+          type="submit"
+          className="btn btn-sm btn-outline btn-success mt-3 pt-0"
+        >
           Save
         </button>
       </form>
+      <ToastContainer position="top-right" />
     </div>
   );
 };
